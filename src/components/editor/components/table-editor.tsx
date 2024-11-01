5be522db6a0ec1cfe9189a6242f76c6bb20e019c
MayaHaynesPrincess48 <166449349+MayaHaynesPrincess48@users.noreply.github.com>
2024-10-31 22:55:57 -0400

    update table-editor

--- src/components/editor/components/table-editor.tsx
+++ src/components/editor/components/table-editor.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
-import { extensions } from '../extensions';  // Make sure you have the table extension imported here
+import { extensions } from '../extensions'; // Make sure you have the table extension imported here
+import { Toolbar } from '../ui/toolbar';
 
 const TableEditor: React.FC = () => {
   const editor = useEditor({
     extensions,
-    content: '<table><tr><th>Header 1</th><th>Header 2</th></tr><tr><td>Row 1, Col 1</td><td>Row 1, Col 2</td></tr></table>',
+    content:
+      '<table><tr><th>Header 1</th><th>Header 2</th></tr><tr><td>Row 1, Col 1</td><td>Row 1, Col 2</td></tr></table>'
   });
 
   return (
     <div>
-      {editor && <Toolbar editor={editor} />}
+      {/* {editor && <Toolbar editor={editor} />} */}
       <EditorContent editor={editor} />
     </div>
   );
